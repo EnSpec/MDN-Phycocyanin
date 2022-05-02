95d37fa8eb2d5923e43e0243e4131bbeb6726874
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-05-02 17:08:35 -0400

    Fixed install script

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -2,7 +2,8 @@ run_dir=$('pwd')
 imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
 
-conda create -y --name sister python=3.8
-
+conda create -y --name sister python=3.7
+source activate sister
+conda install -y tensorflow
 cd $pge_dir
 python setup.py install
