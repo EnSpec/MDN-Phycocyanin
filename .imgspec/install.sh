21d5da7b2b13019111b469a92b41356f956dd823
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-03-20 10:58:14 -0700

    Removed imgspec directory

--- .imgspec/install.sh
+++
@@ -1,9 +0,0 @@
-run_dir=$('pwd')
-imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
-pge_dir=$(dirname ${imgspec_dir})
-
-conda create -y --name sister python=3.7
-source activate sister
-conda install -y tensorflow=1.15.0
-cd $pge_dir
-python setup.py install
