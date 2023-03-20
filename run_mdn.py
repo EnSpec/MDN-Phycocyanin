8dd2e28cbf1cf3ab747349d4688a5054ccd3945e
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-03-20 11:55:27 -0700

    Updated run script

--- run_mdn.py
+++ run_mdn.py
@@ -1,12 +1,17 @@
 import argparse
 import sys
-from MDNphyco import image_estimates
-from MDNphyco.parameters import get_args
-from MDNphyco.utils import set_kwargs_phyco
+from MDNPC import image_estimates
+from MDNPC.parameters import get_args
+from MDNPC.utils import set_kwargs_PC
 import numpy as np
-import hytools_lite as htl
-from hytools_lite.io.envi import WriteENVI, envi_header_dict
+import hytools as ht
+from hytools.io.envi import WriteENVI, envi_header_dict
 from scipy.interpolate import interp1d
+
+try:
+    from osgeo import gdal
+except:
+    import gdal
 
 PRISMA_WAVES = [500, 507, 515, 523, 530,
 				 538, 546, 554, 563, 571,
@@ -35,9 +40,11 @@ def main():
     parser.add_argument('out_dir', type=str,
                           help='Output directory')
 
+    args = parser.parse_args()
+
     out_dir = args.out_dir+'/' if not args.out_dir.endswith('/') else args.out_dir
 
-    rfl = htl.HyTools()
+    rfl = ht.HyTools()
     rfl.read_file(args.rfl_file,'envi')
 
     # frc = gdal.Open(args.frac_cover_file)
@@ -47,7 +54,7 @@ def main():
     #Clear system arguments, needed or else error thrown by MDN function
     sys.argv = [sys.argv[0]]
     sensor = "PRISMA-noBnoNIR"
-    args2 = get_args(set_kwargs_phyco(sensor))
+    args2 = get_args(set_kwargs_PC(sensor))
     print(args2)
 
     phyco = np.zeros((rfl.lines,rfl.columns,1))
@@ -60,8 +67,9 @@ def main():
             interper = interp1d(rfl.wavelengths,chunk)
             hico_chunk = interper(PRISMA_WAVES)
             output, idx = image_estimates(hico_chunk/np.pi,args=args2, sensor=sensor)
+            print(idx)
             phyco[iterator.current_line:iterator.current_line+chunk.shape[0],
-                iterator.current_column:iterator.current_column+chunk.shape[1],0] = output[idx['phyco']][0]
+                iterator.current_column:iterator.current_column+chunk.shape[1],0] = output[idx['PC']][0]
 
     #Mask pixels outside of bounds
     phyco[~mask] = -9999
@@ -88,8 +96,7 @@ def main():
     phyco_header['band names']= ['phycocyanin_mg_m3']
     phyco_header['data ignore value']= -9999
 
-    out_file = "%s/%s_phyco" % (out_dir,rfl.base_name[:-4])
-    out_file = out_file.replace('__','_')
+    out_file = f"{out_dir}/{rfl.base_name}_phyco"
 
     writer = WriteENVI(out_file,phyco_header)
     writer.write_band(phyco[:,:,0],0)
